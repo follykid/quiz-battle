a9cf89b920d0ebf7cfdc0228fe649b72957d5367
follykid <wt113@sshes.tyc.edu.tw>
2026-03-12 20:11:00 +0800

    fix: set base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/quiz-battle/', // 👈 老師，請務必加上這一行，這是您的 Repository 名稱
 })
